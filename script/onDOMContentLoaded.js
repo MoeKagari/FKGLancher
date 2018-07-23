e86fb4f25210c056592021722f4c055acdf65483
MoeKagari <pleasemoemoe@gmail.com>
2018-07-23 15:23:03 +0800

    fixed

--- script/onDOMContentLoaded.js
+++ script/onDOMContentLoaded.js
@@ -1,32 +1,15 @@
 var findFKGAndFitToWindow = function() {
-    if (document.getElementById("game_frame") == null) return;
+    document.body.style.overflow = "hidden";
 
-    var bodyRect = document.body.getBoundingClientRect();
-    if (bodyRect.left == 0 || bodyRect.top == 0) {
-        document.body.style.position = "fixed";
-        document.body.style.overflow = "hidden";
-        document.body.style.margin = "0px";
-        fitToWindow();
+    var game_frame = document.getElementById("game_frame");
+    if (game_frame) {
+        game_frame.style.zIndex = 99;
+        game_frame.style.position = "fixed";
+        game_frame.style.top = "0px";
+        game_frame.style.left = "-" +
+            (Math.round(game_frame.getBoundingClientRect().width) - 960) / 2 +
+            "px";
     }
-}
-
-var fitToWindow = function() {
-    var gameFrameRect = document.getElementById("game_frame").getBoundingClientRect();
-    var bodyRect = document.body.getBoundingClientRect();
-
-    var offsetX = Math.round(gameFrameRect.left) + (Math.round(gameFrameRect.width) - 960) / 2;
-    var offsetY = Math.round(gameFrameRect.top) //
-        +
-        0 //gameFrame 上方的空白
-        -
-        0 //gameFrame 之后会resize,body上方与gameFrame上方缩短了19
-    ;
-
-    var destinationX = 0 - offsetX;
-    var destinationY = 0 - offsetY;
-
-    document.body.style.left = "" + destinationX + "px";
-    document.body.style.top = "" + destinationY + "px";
 }
 
 findFKGAndFitToWindow();
